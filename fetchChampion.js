e118a9cafe6b35f6b7828ff75e21325a98819559
lucasmarinari <lucasmarinari7@gmail.com>
2025-03-14 17:43:03 -0300

    Configuracao hover nos campeoes

--- fetchChampion.js
+++ fetchChampion.js
@@ -13,17 +13,21 @@ async function fetchChampion() {
 
         championList.forEach((champion) => {
             const li = document.createElement('li');
-            li.classList.add('flex', 'flex-col', 'items-center', 'p-4', 'w-32', 'h-56', 'border-2', 'border-', 'rounded-lg', 'color-blue-5', 'shadow-md', 'm-4');
+            li.classList.add('flex', 'flex-col', 'items-center', 'p-4', 'w-32', 'h-56', 'border-2', 'rounded-lg', 'color-blue-5', 'shadow-lg', 'm-4');
             li.style.marginTop = "1rem";
-            
+            li.style.borderColor = 'var(--color-gold-5)';
+            li.style.cursor = 'pointer';
+            li.style.overflow = "hidden";
             const img = document.createElement('img');
             img.src = `https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.id}.png`;
             img.alt = champion.name;
             img.classList.add('w-20', 'h-20', 'object-cover');
+            img.style.marginBootom = "2rem";
 
             const span = document.createElement('span');
             span.textContent = champion.name;
-            span.classList.add('text-center', 'text-lg', 'font-semibold', 'text-black');
+            span.classList.add('text-center', 'text-lg', 'font-semibold', 'text-white');
+
 
             li.appendChild(img);
             li.appendChild(span);
